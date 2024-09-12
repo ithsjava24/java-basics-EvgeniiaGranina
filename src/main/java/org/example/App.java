1d6f768cee0386bab295a366fb8344d6681e26ec
EvgeniiaGranina <evgeniia.granina@iths.se>
2024-09-12 12:40:19 +0200

    Add menu och avslutning

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+
+        System.out.print("Elpriser\n" +
+                "========\n" +
+                "1. Inmatning\n" +
+                "2. Min, Max och Medel\n" +
+                "3. Sortera\n" +
+                "4. Bästa Laddningstid (4h)\n" +
+                "5. Visualisering\n" +
+                "e. Avsluta\n");
+        Scanner sc = new Scanner(System.in);
+        String choice = sc.nextLine();
+        if (choice == "e") {
+            return;
+        }
+
     }
 }
