46c934cb82621c87ae530148a19ca7f9f9543796
EvgeniiaGranina <evgeniia.granina@iths.se>
2024-09-20 07:54:02 +0200

    Add input, minMaxMean, constructor TimePris

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,55 +1,117 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class App {
-    private static Scanner sc = new Scanner(System.in);
+    private static final TimPris[] price = new TimPris[24];
+//    public final static int ROW_COUNT = 10;
+//    public static int[] prices;
+//    public static Random random = new Random();
 
     public static void main(String[] args) {
-        int[] pris = {0, 0, 0, 0, 52, 256, 403, 762, 920, 768, 1123, 972, 569, 620, 647, 691, 826, 1033, 2278, 2511, 1144, 920, 690, 570};
-
-        System.out.print("Elpriser\n" +
-                "========\n" +
-                "1. Inmatning\n" +
-                "2. Min, Max och Medel\n" +
-                "3. Sortera\n" +
-                "4. Bästa Laddningstid (4h)\n" +
-                "5. Visualisering\n" +
-                "e. Avsluta\n");
         Scanner sc = new Scanner(System.in);
-        String choice = sc.nextLine();
-        if (choice.equalsIgnoreCase("e")) {
-            return;
-        } else if (choice.equals("1")) {
-            System.out.print("Vilka timmar vill du veta om priserna?\n");
-            int hour = sc.nextInt();
-            System.out.println(pris[hour]);
-        } else if (choice.equalsIgnoreCase("2")) {
-            int min = Math.min(pris[]);
-            int max = Math.max(pris[]);
-            int count = 24;
-            int sum = 0;
-            for (int i = 0; i < pris.length; i++) {
-                sum +=;
+//        prices = inputPrice();
+//        printAllLines(prices);
+//        printHours(prices);
+        String textMenu = """
+                Elpriser
+                ========
+                1. Inmatning
+                2. Min, Max och Medel
+                3. Sortera
+                4. Bästa Laddningstid (4h)
+                5. Visualisering
+                e. Avsluta
+                """;
+        while (true) {
+
+            System.out.print(textMenu);
+
+            String choice = sc.nextLine();
+            if (choice.equalsIgnoreCase("e")) {
+                return;
+            } else if (choice.equals("1")) {
+                input(sc);
+
+            } else if (choice.equalsIgnoreCase("2")) {
+                minMaxMean();
+//                int min = Arrays.stream(pris).min().getAsInt();
+//                int max = Arrays.stream(pris).max().getAsInt();
+//                int count = 24;
+//                int sum = 0;
+//                for (int i = 0; i < pris.length; i++)
+//                    sum += pris[i];
+//
+//                int medel = sum / count;
+//                System.out.print("Min pris är: " + min + "\nMax pris är: " + max + "\nMedel pris är: " + medel + "\n");
             }
-            int medel = sum / count;
-            System.out.print("Min pris är: " + min + "\nMax pris är: " + max + "\nMedel pris är: " + medel + "\n");
+            //
+            else if (choice.equalsIgnoreCase("5")) ;
         }
+    }
+
+    private static class TimPris {
+        private final String tim;
+        private final int pris;
+
+        TimPris(String tim, int pris) {
+            this.tim = tim;
+            this.pris = pris;
+        }
+
+        public String getTim() {
+            return tim;
+        }
+
+        public int getPris() {
+            return pris;
+        }
+    }
+
+    private static void input(Scanner scanner) {
+        System.out.print("Mata in priser för varie timme (i öre):\n");
+        for (int i = 0; i < price.length; i++) {
+            System.out.printf("%02d-%02d: ", i, i + 1);
+            int val = Integer.parseInt(scanner.nextLine());
+            price[i] = new TimPris(String.format("%02d-%02d ", i, i + 1), val);
+        }
+    }
+
+    private static void minMaxMean() {
+
+        long count = 0;
+        long sum = 0;
+        TimPris min = new TimPris("N/A", Integer.MAX_VALUE);
+        TimPris max = new TimPris("N/A", Integer.MIN_VALUE);
+
+        for (TimPris timPris : price) {
+            if (timPris.getPris() < min.getPris()) {
+                min = timPris;
+            }
+            if (timPris.getPris() > max.getPris()) {
+                max = timPris;
+            }
+            ++count;
+            sum += timPris.pris;
+        }
+        System.out.printf("Lägsta pris: %s, %d öre/kWh\n", min.tim, min.pris);
+        System.out.printf("Högsta pris: %s, %d öre/kWh\n", max.tim, max.pris);
+        System.out.printf("Medelpris: %.2f öre/kWh\n", count > 0 ? (double) sum / count : 0.0d);
+
+
+//        System.out.print("Lägsta pris: " + stat.getMin() + " öre/kWh" +
+//                "\nHögsta pris: " + stat.getMax() + " öre/kWh");
+//        System.out.print("\nMedel pris: " + String.format("%.2f", (float) stat.getSum() / price.length) + " öre/kWh\n");
+
     }
 
 }
 
-//    public static int MinMaxMedel(){
-//        int min = Math.min(pris[]);
-//        int max = Math.max(pris[]);
-//        int count = 24;
-//        int sum = 0;
-//        for (int i = 0; i < pris.length; i++) {
-//            sum+=;
-//        }
-//        int medel = sum/count;
-//        return min;
-//        return max;
-//        return medel;
-//    }
 
